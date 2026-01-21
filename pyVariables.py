b91f204b72ce03264b5d51a0ebd4de80398b4e7c
mavmav <maumandac@gmail.com>
2026-01-21 22:19:04 +1300

    Add boolean practice

---
+++ pyVariables.py
@@ -0,0 +1,41 @@
+name = "Colt"
+age = 29
+#comment sample
+
+# or
+# and
+# not 
+
+# is 
+# in
+
+# def is_adult(age):
+#     return True if age > 18 else False
+
+#can use "" double quotes or singe quotes ''
+# + can be used to concatenate 
+
+#phrase = name + "is the name."
+
+name += " is my name " #alternative to concatenate
+print(name)
+
+print("""
+      Hi
+      Hello 
+      World
+
+      """) #can use triple quotes to make multi line string
+
+name = "Be\au" #use backlash to include another quote
+
+name = "Benny is cool" #new line
+print(name)
+
+print(name[1:5])
+
+done = False
+if done:
+    print("Yes")
+else:
+    print("NO")
